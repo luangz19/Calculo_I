6609f6e3c2459cd0efb7f80c4c169f703c3cbf10
luangz19 <luangonzaga119@gmail.com>
2025-05-11 13:43:05 -0400

    ola

---
+++ Derivadas/1.0-Tangentes.py
@@ -0,0 +1,22 @@
+from pylab import*
+from numpy import*
+from sympy import*
+
+x = Symbol('x')
+
+f = x**2
+
+g = x**2 - 1
+
+# x = linspace(-10,10,100)
+# y = f(x)
+
+# g = (f(x) - 1) / (x -1)
+
+
+
+# fig = figure(figsize=(10,10))
+# plot(x,y)
+# plot(x,g(x))
+# grid()
+# show()
